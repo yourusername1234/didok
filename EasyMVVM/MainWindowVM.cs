534beb0c0d6c6a6031f5606db4ac032974d53ade
yourusername1234 <32257728+yourusername1234@users.noreply.github.com>
2020-06-06 18:37:36 +0300

    Add files via upload

---
+++ EasyMVVM/MainWindowVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+
+namespace EasyMVVM
+{
+    public class MainWindowVM : DependencyObject, INotifyPropertyChanged
+    {
+        public MainWindowVM()
+        {
+            Model m = new Model();
+            BoundProperty = m.GetData();
+        }
+
+        //This event will be fired to notify any listeners of a change in a property value.
+        public event PropertyChangedEventHandler PropertyChanged;
+        //Tell WPF Binding that this property value has changed
+        public void PropChanged(String propertyName)
+        {
+            //Did WPF registerd to listen to this event
+            if (PropertyChanged != null)
+            {
+                //Tell WPF that this property changed
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private ObservableCollection<string> _BackingProperty;
+        public ObservableCollection<string> BoundProperty
+        {
+            get { return _BackingProperty; }
+            set { _BackingProperty = value; PropChanged("BoundProperty"); }
+        }
+
+
+    }
+}
